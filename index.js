257411ac06f8954841a75259ac3a777edbcdbb74
Sander Peterse <sander.peterse88@gmail.com>
2020-08-22 19:45:29 +0200

    Fixes #215

--- index.js
+++ index.js
@@ -35,9 +35,6 @@
 // }
 //
 const Helper = require('./lib/helper').Helper;
-const eDomoticzServices = require('./lib/services').eDomoticzServices;
-const eDomoticzPlatform = require('./lib/domoticz_platform');
-const eDomoticzTVPlatform = require('./lib/domoticz_tv_platform');
 
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
@@ -45,40 +42,8 @@ module.exports = function(homebridge) {
     Types = homebridge.hapLegacyTypes;
     UUID = homebridge.hap.uuid;
 
-    Helper.fixInheritance(eDomoticzServices.TotalConsumption, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.CurrentConsumption, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.GasConsumption, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.TempOverride, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.MeterDeviceService, Service);
-    Helper.fixInheritance(eDomoticzServices.GasDeviceService, Service);
-    Helper.fixInheritance(eDomoticzServices.Ampere, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.AMPDeviceService, Service);
-    Helper.fixInheritance(eDomoticzServices.Volt, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.VOLTDeviceService, Service);
-    Helper.fixInheritance(eDomoticzServices.CurrentUsage, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.UsageDeviceService, Service);
-    Helper.fixInheritance(eDomoticzServices.TodayConsumption, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.Barometer, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.WaterFlow, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.TotalWaterFlow, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.WaterDeviceService, Service);
-    Helper.fixInheritance(eDomoticzServices.WeatherService, Service);
-    Helper.fixInheritance(eDomoticzServices.WindSpeed, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.WindChill, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.WindDirection, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.WindDeviceService, Service);
-    Helper.fixInheritance(eDomoticzServices.Rainfall, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.RainDeviceService, Service);
-    Helper.fixInheritance(eDomoticzServices.Visibility, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.VisibilityDeviceService, Service);
-    Helper.fixInheritance(eDomoticzServices.SolRad, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.SolRadDeviceService, Service);
-    Helper.fixInheritance(eDomoticzServices.LocationService, Service);
-    Helper.fixInheritance(eDomoticzServices.Location, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.InfotextDeviceService, Service);
-    Helper.fixInheritance(eDomoticzServices.Infotext, Characteristic);
-    Helper.fixInheritance(eDomoticzServices.UVDeviceService, Service);
-    Helper.fixInheritance(eDomoticzServices.UVIndex, Characteristic);
+    const eDomoticzPlatform = require('./lib/domoticz_platform');
+    const eDomoticzTVPlatform = require('./lib/domoticz_tv_platform');
 
     homebridge.registerPlatform("homebridge-edomoticz", "eDomoticz", eDomoticzPlatform, true);
     homebridge.registerPlatform("homebridge-edomoticz-tv", "eDomoticzTV", eDomoticzTVPlatform, true);
